03cbac64e85e24b19c43b686bc0cd718843abf02
Srinivasa Prithvi K S <96895029+srinivasaprithvi25@users.noreply.github.com>
2025-06-07 12:32:09 +0530

    Add optional extra columns to data fetcher

--- PredictionOpenAI/app/utils/DataFetcher.py
+++ PredictionOpenAI/app/utils/DataFetcher.py
@@ -1,14 +1,19 @@
 import pandas as pd
 from utils.DatabaseConnection import get_db_engine
 
-def fetch_data(query_info):
+def fetch_data(query_info, extra_columns=None):
     engine = get_db_engine()
     date_col = query_info['date_column']
     target_col = query_info['target_column']
     table = query_info['table']
     filters = query_info.get('filters', '')
 
-    sql = f"SELECT {date_col}, {target_col}, Customercode FROM {table}"
+    columns = [date_col, target_col, 'Customercode']
+    if extra_columns:
+        columns.extend(extra_columns)
+
+    col_str = ', '.join(columns)
+    sql = f"SELECT {col_str} FROM {table}"
     if filters:
         sql += f" WHERE {filters}"
 
